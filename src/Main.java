0c53a74f11f6faaed7c973b2411fe969f9d6b279
Dima Zap <zapilenovd@gmail.com>
2021-12-06 20:22:55 +0300

    numbertic delete

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int ticket = 25;
+        int ticket = 1800;
         int mils = service.calculate(ticket);
         System.out.println(mils);
     }
